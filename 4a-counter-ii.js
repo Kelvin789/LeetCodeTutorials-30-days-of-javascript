3d8e80d3bceb7e1df8adaac6aeb52f632c42f22a
Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
2025-04-27 12:43:16 +0100

    Create 4a-counter-ii.js

---
+++ 4a-counter-ii.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/counter-ii - JavaScript
+
+function createCounter(init) {
+    let currentValue = init;
+
+    return {
+        increment: function() {
+            currentValue += 1;
+            return currentValue;
+        },
+        decrement: function() {
+            currentValue -= 1;
+            return currentValue;
+        },
+        reset: function() {
+            currentValue = init;
+            return currentValue;
+        }
+    }
+};
