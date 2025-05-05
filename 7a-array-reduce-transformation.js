8488d3da83ad7f618916a5f58cfdb1d1e3b073f2
Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
2025-05-05 11:18:24 +0100

    Create 7a-array-reduce-transformation.js

---
+++ 7a-array-reduce-transformation.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/array-reduce-transformation - JavaScript
+
+var reduce = function(nums, fn, init) {
+    let result = init;
+
+    for (let i = 0; i < nums.length; i++) {
+        result = fn(result, nums[i])
+    }
+
+    return result;
+};
