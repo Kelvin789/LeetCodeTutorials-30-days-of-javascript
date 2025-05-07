43700d2539aeaabf8eacb3d8b5ff0a7c94c6d9c9
Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
2025-05-07 23:22:53 +0100

    Update 7b-array-reduce-transformation.ts

--- 7b-array-reduce-transformation.ts
+++ 7b-array-reduce-transformation.ts
@@ -6,7 +6,7 @@ function reduce(nums: number[], fn: Fn, init: number): number {
     let result = init;
 
     for (let i = 0; i < nums.length; i++) {
-        result = fn(result, nums[i])
+        result = fn(result, nums[i]);
     }
 
     return result;
