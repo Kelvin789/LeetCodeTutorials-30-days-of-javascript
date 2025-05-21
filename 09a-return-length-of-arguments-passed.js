659874ab197377cffd9e62fd4e7a6a9041525d63
Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
2025-05-21 23:27:17 +0100

    Create 09a-return-length-of-arguments-passed.js

---
+++ 09a-return-length-of-arguments-passed.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/return-length-of-arguments-passed - JavaScript
+
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
