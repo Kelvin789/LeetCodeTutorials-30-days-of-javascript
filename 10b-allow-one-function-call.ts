260c77160c257345b2a8a198cf43d3629048e8b9
Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
2025-05-22 23:45:11 +0100

    Create 10b-allow-one-function-call.ts

---
+++ 10b-allow-one-function-call.ts
@@ -0,0 +1,27 @@
+ // https://leetcode.com/problems/allow-one-function-call - TypeScript
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let hasBeenCalled = false;
+    let result;
+
+    return function(...args){
+        if (!hasBeenCalled) {
+            hasBeenCalled = true;
+            result = fn(...args);
+            return result;
+        } else {
+            return undefined;
+        }
+    }
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
