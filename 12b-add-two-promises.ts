6cd59a0cc23d48f8d07f21652597d0c785315fa0
Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
2025-06-24 23:00:18 +0100

    Create 12b-add-two-promises.ts

---
+++ 12b-add-two-promises.ts
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/add-two-promises - TypeScript
+
+type P = Promise<number>
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+    return Promise.all([promise1,promise2])
+           .then(([value1, value2]) => value1 + value2);
+};
+
+/** Debugging:
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
